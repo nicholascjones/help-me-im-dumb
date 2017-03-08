534618f5506c6e416090236669ae0ed78c5ef833
cshinaver <bshinaver@gmail.com>
2017-03-07 22:11:39 -0500

    added -index main arg

--- src/help_me_im_dumb/core.clj
+++ src/help_me_im_dumb/core.clj
@@ -1,7 +1,19 @@
 (ns help-me-im-dumb.core
+  (:require [help-me-im-dumb.index :as index])
   (:gen-class))
+
+(defn usage
+  []
+  (println "usage: OPTIONS")
+  (println "\t-index json-data-file\tIndexes json data file"))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (cond
+    (nil? args) (usage)
+    (= "-index" (first args))
+    (do
+      (index/create-indicies (first args))
+      (println "Index created successfully!"))
+    :else (usage)))
