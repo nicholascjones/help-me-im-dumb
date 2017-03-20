7977ba7be90e26160d4e213093b9d85587bda481
cshinaver <bshinaver@gmail.com>
2017-03-20 00:27:27 -0400

    Added boolean query matcher

---
+++ src/help_me_im_dumb/query-engine.clj
@@ -0,0 +1,18 @@
+(ns help-me-im-dumb.query-engine
+  (:require [help-me-im-dumb.index :as index]))
+
+(declare boolean-query-matcher)
+
+(defn query->results
+  [matcher q]
+  (map index/docid->url (matcher q)))
+
+(defn boolean-query-matcher
+  "Super dumb query matcher. Returns all docs with all words"
+  [q]
+  (apply
+   clojure.set/intersection
+   (map
+    #(into #{} (index/term->postings %))
+    (clojure.string/split q #"\s+"))))
+
