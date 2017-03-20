658d427a494ad78f84de4e82fdd56843485054b8
cshinaver <bshinaver@gmail.com>
2017-03-20 01:15:47 -0400

    /query/:q hosted

--- src/help_me_im_dumb/handler.clj
+++ src/help_me_im_dumb/handler.clj
@@ -3,13 +3,20 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.adapter.jetty :as jetty]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.data.json :as json]
+            [help-me-im-dumb.query-engine :as query]))
 
 (defn read-template [filename]
   (io/resource filename))
 
+(defn boolean-query-results
+  [q]
+  (json/write-str (query/bquery->results q)))
+
 (defroutes app-routes
   (GET "/" [] (read-template "templates/index.html"))
+  (GET "/query/:q" [q] (boolean-query-results q))
   (route/not-found "Not Found"))
 
 (def app
@@ -19,3 +26,5 @@
   "Runs jetty server on given port"
   [port]
   (jetty/run-jetty #'app {:port port :join? false}))
+
+;; (def server (run-server 5000))
