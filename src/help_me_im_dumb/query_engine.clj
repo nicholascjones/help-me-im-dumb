d34dd8c5df5b1f43e335bd2d52b5ff3601017a1b
Jared <jrodjrod25@yahoo.com>
2017-03-22 22:42:29 -0400

    added 3 functions that work toward implementing tfidf

--- src/help_me_im_dumb/query_engine.clj
+++ src/help_me_im_dumb/query_engine.clj
@@ -19,3 +19,34 @@
       q-tokens))))
 
 (def bquery->results (partial query->results boolean-query-matcher))
+
+
+
+(defn list-all-docs
+  "for a given query list all documents that at least one appears in"
+  [q]
+  (apply clojure.set/union
+         (distinct
+           (map
+             #(into #{} (index/term->postings %))
+             (clojure.string/split q #"\s+")
+             ))))
+
+
+(defn idf-score
+  "for a given docs and a term find idf"
+  [term docs]
+  (/
+    (float (count docs))
+    (float (count (index/term->postings term)))
+    )
+  )
+
+
+(defn tf-score
+  "given 1 doc and 1 term find term freq"
+  [term docid]
+  (count
+    (re-seq(re-pattern term)
+            (get (index/docid->doc docid) :title))))
+
